9cc492cad2a4d98921c1fd655ad1c48899b34e97
isabellavarelaa <727144@student.dusd.net>
2024-11-14 14:47:32 -0800

    workin on fixing prefabs
    making changes

--- 2DBeginnerAdventureP8Varela/Assets/Scripts/CandyController.cs
+++ 2DBeginnerAdventureP8Varela/Assets/Scripts/CandyController.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class CandyController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
-   public int health { get { return currentHealth; } }
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
+
 
     }
 
@@ -28,10 +32,19 @@ public class CandyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+
     }
     void FixedUpdate()
     {
-
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
@@ -41,6 +54,15 @@ public class CandyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (!isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
