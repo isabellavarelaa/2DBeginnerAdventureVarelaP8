b17a535a82b41e418170a394db5a857b40048208
isabellavarelaa <727144@student.dusd.net>
2024-11-20 15:25:40 -0800

    Adding animation
    adding animation to my player and its enemie

--- 2DBeginnerAdventureP8Varela/Assets/Scripts/CandyController.cs
+++ 2DBeginnerAdventureP8Varela/Assets/Scripts/CandyController.cs
@@ -18,11 +18,15 @@ public class CandyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
 
     }
@@ -32,6 +36,15 @@ public class CandyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+       Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
 
         if (isInvincible)
         {
