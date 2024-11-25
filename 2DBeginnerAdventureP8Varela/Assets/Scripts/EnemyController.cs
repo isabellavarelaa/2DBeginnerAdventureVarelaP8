3de0e19980fe5a5ffa324d6138bed54dbd73c6c9
isabellavarelaa <727144@student.dusd.net>
2024-11-25 01:26:29 -0800

    changing
    chnging

--- 2DBeginnerAdventureP8Varela/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Varela/Assets/Scripts/EnemyController.cs
@@ -10,7 +10,7 @@ public class EnemyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
-    bool broken;
+    bool broken = true;
 
     float timer;
     int direction = 1;
@@ -78,5 +78,6 @@ public class EnemyController : MonoBehaviour
     {
         broken = false;
         rigidbody2d.simulated = false;
+        animator.SetTrigger("Fixed");
     }
 }
